0533a314f1faf17181ab2e91b9ff6a8aeca36201
bitale-hash <bitontiale@gmail.com>
2026-04-09 10:15:40 +0000

    es1

---
+++ Dave.java
@@ -0,0 +1,10 @@
+package Java;
+//esercizio1
+public class Dave {
+    public void PrintDave(){
+        System.out.println("+ - - +");
+        System.out.println("| Dave |");
+        System.out.println("+ - - +");
+    }
+    
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+package Java;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        a=1;
+        System.out.println(" a= "+a);
+
+        Dave d=new Dave();
+        d.PrintDave();
+    }
+}
